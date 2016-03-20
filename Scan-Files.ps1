cdf7f8331be6933c34f8eac01ba8c2e62954513a
ISJ-439 <adamther2@gmail.com>
2016-03-20 01:28:17 -0700

    Cut Down Code

--- Scan-Files.ps1
+++ Scan-Files.ps1
@@ -1,30 +1,10 @@
-﻿# Folder section windows declaration
-Add-Type -AssemblyName System.Windows.Forms
-$FolderBrowser = New-Object System.Windows.Forms.FolderBrowserDialog
+﻿# Paramiters For Script
+$NetworkDumpDir = 'Z:\ScanLogs'
 
-function Scan-Files($FolderPath){
-    [string]$TimeStamp = Get-Date -UFormat '%y%m%d-%H%M%S'
-    [string]$LogFile = "$PSScriptRoot\Scan-Files-Results-(YYMMDD-HHMMSS)-($TimeStamp)"+".csv"
-    if(Test-Path "$PSScriptRoot\sigcheck.exe"){
-        Write-Host "Found sigcheck, starting a recursive scan of executable objects only..." -fo Green
-        Start-Process "$PSScriptRoot\sigcheck.exe" -Wait -ArgumentList " -c -u -e -s -vt -vs `"$FolderPath`"" -RedirectStandardOutput $LogFile
-        Import-Csv -LiteralPath $LogFile | ogv
-    }
-    else{
-        Write-Host "FAILURE: Cannot locate sigcheck.exe !!!" -NoNewline -fo Red 
-        Write-Host " Download a copy from https://technet.microsoft.com/en-us/sysinternals/bb897441.aspx and place it in the script directory." 
-    }
-}
+# Initialize Variable Paramiters\
+[string]$PCName = $env:COMPUTERNAME
+[string]$TimeStamp = Get-Date -UFormat '%y%m%d-%H%M%S'
+[string]$LogFile = "$NetworkDumpDir\Scan-Files-Results-(YYMMDD-HHMMSS)-($TimeStamp)_For($PCName)"+".csv"
 
-# Call folder window dialog
-[void]$FolderBrowser.ShowDialog()
-
-# Prevent canceled selection from causing a malfunction
-if ($FolderBrowser.SelectedPath -like "*:*"){
-    Scan-Files ($FolderBrowser.SelectedPath)
-}
-
-else {
-    Write-Host "No folder selected..." -fo Red
-    exit
-}
+# Start Process
+Start-Process "C:\Scan-Files\sigcheck.exe" -Wait -ArgumentList " -c -u -e -s -vt -vs `"C:\`"" -RedirectStandardOutput $LogFile
